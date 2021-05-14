58c80fb255a4ccc8763323e48a2a2ca486bac758
Onur <onurozkan.dev@outlook.com>
2021-05-14 19:37:00 +0300

    add keyboard layout block

--- config.h
+++ config.h
@@ -2,16 +2,17 @@
 
 static const Block blocks[] = {
 	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{ "dwm_memory",		3,		0	},
 	{ "dwm_kernel",		0,		0	},
 	{ "dwm_cpu_temp",	    3,		0	},
 	// { "dwm_date",		    3600,	0	},
-	{ "dwm_memory",		3,		0	},
 	{ "dwm_brightness",	0,		11	},
 	{ "dwm_volume",		0,		10	},
 	{ "dwm_network",	    3,	    0   },
 	// { "dwm_traffic",	    1,	    0   },
 	{ "dwm_battery",		3,		0	},
 	{ "dwm_clock",		3,		0	},
+	{ "dwm_keyboard_layout",	0,		12	},
 	{ "dwm_uptime",		60,		0	},
 };
 
