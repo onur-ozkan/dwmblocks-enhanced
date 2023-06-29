1b8b351b43e3322f84e8a5bd9a174663fc9f8731
ozkanonur <work@onurozkan.dev>
2023-06-29 19:28:04 +0300

    enable `dwm_disk` script
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- config.h
+++ config.h
@@ -2,14 +2,15 @@
 
 static const Block blocks[] = {
 	/* Command *//* Update Interval *//* Update Signal */
+	{"dwm_traffic", 1, 0 },
 	{"dwm_memory", 3, 0},
+	{"dwm_disk", 5, 0},
 	{"dwm_kernel", 0, 0},
 	{"dwm_cpu_temp", 3, 0},
 	// { "dwm_date", 3600, 0 },
 	{"dwm_brightness", 0, 11},
 	{"dwm_volume", 0, 10},
 	{"dwm_network", 3, 0},
-	// { "dwm_traffic", 1, 0 },
 	{"dwm_battery", 3, 0},
 	{"dwm_clock", 3, 0},
 	{"dwm_keyboard_layout", 0, 12},
